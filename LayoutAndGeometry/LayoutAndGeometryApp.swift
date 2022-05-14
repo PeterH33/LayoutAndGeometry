5a94b994d6496c1a11575cb64053c1acde4a48be
Peter Hartnett <peter.hartnett@gmail.com>
2022-05-14 12:27:50 -0400

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Peter Hartnett on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
